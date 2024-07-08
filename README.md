85952819d063d2736d9e6dbd652aef522e226acf
Po-Ting Lin <im.poting.lin@gmail.com>
2024-07-08 16:37:25 +0200

    support output as a file

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # Project-map
-
 A Python script to scan and visualize the directory structure of a project, with options to exclude specific folders.
 
-## Features
 
+## Features
 - Scans and prints the directory hierarchy of a specified project.
 - Allows exclusion of specific folders from the output.
 - Simple command-line interface.
+- Saves the output to a text file.
 
 ## Installation
-
 Clone the repository:
 ```sh
 git clone https://github.com/poting-lin/project-map.git
@@ -17,11 +16,15 @@ cd project-map
 ```
 
 ## Usage
-
 Run the script using the following command:
 
 ```sh
-python project-map/scan.py --path path_to_your_project_directory --exclude folder_to_exclude1 folder_to_exclude2 ...
+python project-map/scan.py --path path_to_your_project_directory --exclude folder_to_exclude1 folder_to_exclude2 --output output_file.txt
+```
+
+Or using short arguments:
+```sh
+python project-map/scan.py -p path_to_your_project_directory -e folder_to_exclude1 folder_to_exclude2 -o output_file.txt
 ```
 
 ### Output
--- project-map/scan.py
+++ project-map/scan.py
@@ -2,17 +2,24 @@ import os
 import argparse
 
 
-def scan_directory(directory, exclude_folders=None, indent=0):
+def scan_directory(directory, exclude_folders=None, indent=0, output_lines=None):
     if exclude_folders is None:
         exclude_folders = []
+    if output_lines is None:
+        output_lines = []
 
     for item in os.listdir(directory):
         item_path = os.path.join(directory, item)
         if item in exclude_folders:
             continue
-        print(' ' * indent + '├── ' + item)
+        line = ' ' * indent + '├── ' + item
+        print(line)
+        output_lines.append(line)
         if os.path.isdir(item_path):
-            scan_directory(item_path, exclude_folders, indent + 4)
+            scan_directory(item_path, exclude_folders,
+                           indent + 4, output_lines)
+
+    return output_lines
 
 
 def main():
@@ -22,15 +29,21 @@ def main():
                         help="The path to the project directory.")
     parser.add_argument('--exclude', '-e', nargs='*', default=[],
                         help="List of folder names to exclude.")
+    parser.add_argument('--output', '-o', type=str, default='output.txt',
+                        help="The file to save the output (default: output.txt).")
 
     args = parser.parse_args()
 
     project_dir = args.path
     exclude_folders = args.exclude
+    output_file = args.output
 
     if os.path.exists(project_dir) and os.path.isdir(project_dir):
         print(project_dir + '/')
-        scan_directory(project_dir, exclude_folders)
+        output_lines = [project_dir + '/']
+        output_lines.extend(scan_directory(project_dir, exclude_folders))
+        with open(output_file, 'w') as f:
+            f.write('\n'.join(output_lines))
     else:
         print(
             f"The directory {project_dir} does not exist or is not a directory.")
